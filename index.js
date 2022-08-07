3d323ab7bdc7c3b73b4b2e52d83207407aa6f29a
i-guerrero <isaacguerrero@pursuit.org>
2022-08-07 09:38:46 -0400

    Add filterByGenre() to index.js

--- index.js
+++ index.js
@@ -101,7 +101,12 @@ function findById(movies, id) {
  *  filterByGenre(movies, "Horror")
  *  //> []
  */
-function filterByGenre() {}
+function filterByGenre(movies, genre) {
+  if (!movies.length) throw "Movies array is empty";
+  return movies.filter((movie) =>
+    movie.genre.toLowerCase().includes(genre.toLowerCase())
+  );
+}
 
 /**
  * getAllMoviesReleasedAtOrBeforeYear()
