32419facbb59a1272ea4f88cc8f43b3f0711d534
i-guerrero <isaacguerrero@pursuit.org>
2022-08-07 09:29:09 -0400

    Add checkIfAnyMovieHasRating() to index.js

--- index.js
+++ index.js
@@ -53,7 +53,10 @@ function getAllMovieTitles(movies) {
  *  checkIfAnyMovieHasRating(movies, "R");
  *  //> false
  */
-function checkIfAnyMovieHasRating() {}
+function checkIfAnyMovieHasRating(movies, rating = "G") {
+  if (!movies.length) throw "Movies array is empty";
+  return movies.some((movie) => movie.rated === rating);
+}
 
 /**
  * findById()
