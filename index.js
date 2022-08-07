8e8dde9887145afb2cbec8c4bd572632e5931ae8
ayd823 <aydiaz823@gmail.com>
2022-08-07 10:25:09 -0400

    checkIfAnyMovieHasRating

--- index.js
+++ index.js
@@ -60,7 +60,13 @@ function getAllMovieTitles(movies) {
  *  checkIfAnyMovieHasRating(movies, "R");
  *  //> false
  */
-function checkIfAnyMovieHasRating() {}
+function checkIfAnyMovieHasRating(movies, rating = "G") {
+  if (movies.length === 0) {
+    throw "Error";
+  }
+
+  return movies.some(({ rated }) => rated === rating);
+}
 
 /**
  * findById()
